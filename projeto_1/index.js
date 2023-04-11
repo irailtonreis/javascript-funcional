b7ba110a0355fa29fc91d512469f6ceae041ca80
irailtonreis <irailton.oliveira91@gmail.com>
2023-04-11 08:58:55 -0300

    feat: Iniciando projeto extração legendas

---
+++ projeto_1/index.js
@@ -0,0 +1,8 @@
+
+const path = require('path')
+const fn = require('./funcoes')
+
+const caminho = path.join(__dirname, '..', 'dados', 'legendas')
+
+const arquivos = fn.lerDiretorio(caminho)
+console.log(arquivos)
