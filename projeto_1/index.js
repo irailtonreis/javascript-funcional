c8136960bed269545fd4a11d3aabf1bd98581fd4
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-16 11:33:10 -0300

    feat: Leitura do conteúdo dos arquivos

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -6,4 +6,5 @@ const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
 fn.lerDiretorio(caminho)
 .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+.then(arquivosSTR => fn.lerArquivos(arquivosSTR))
 .then(console.log)
